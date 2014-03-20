55d0803691782ebaf0cc9b028e76608690eacab7
hiluxzero <info@jorgemora.cl>
2014-03-20 14:34:33 -0400

    Helpers Html 1.0 OK

--- app/helpers/load_helper.php
+++ app/helpers/load_helper.php
@@ -3,54 +3,12 @@
 # filename: load_helper.php
 # Helper para crear código HTML estandarizado
 #------------------------------------------------------------------------------------
+class LoadHtml {
 
-
-class loads {
-
-	/*
-		example: $this->Html->stylesheet->('css/app.css', array('rel' => 'stylesheet','type' => 'text/css'));
-		return: <link rel="stylesheet" type="text/css" href="css/app.css" />
-
-		$url = dirección de la hoja de estilo
-		$options = rel, type
-
-	*/
- 
-	function css($url,$options => array()) {
-		#----------------------------------------
-		# Carga una hoja de estilo en particular
-		#----------------------------------------
+	function css($url) {
 		if (isset($url)) { 
-			if (!is_array($options)) { 
-				$rel = 'stylesheet';
-				$type = 'text/css';
-			} else {
-				if (!isset($options['rel']) {
-					$rel = 'stylesheet';
-				} else if ($options['rel'] == strtolower('stylesheet')) {
-					$rel = strtolower($options['rel'])
-				}
-
-				if (!isset($options['type']) {
-					$type = 'text/css';
-				} else if ($options['rel'] == strtolower('stylesheet')) {
-					$type = 'text/css';
-				}
-			}
-
-			$link = '<link rel="'.$rel.'" type="'.$type.'" href="'._CSS_.$url.'" />';
-			return $link;
-
-		#-----------------------------------
-		# Carga todas las hojas de estilos 
-		#-----------------------------------
-
-		} else {
-			$files = array_diff(scandir(__CSS__), array('..', '.'));!= "") $items[] = $i;
-			foreach ($files as $f):
-				$link += '<link rel="stylesheet" type="text/css" href="'__CSS__.$f.'" /><br/>';
-			endforeach;
-			return $link;
+			$link = '<link href="'._CSS_.$url.'.css" rel="stylesheet" type="text/css">';
+			echo $link;
 		}
 	}
 
@@ -61,62 +19,25 @@ class loads {
 		$url = dirección de la librería		
 	*/
 	function javascript($url) {
-		#----------------------------------------
-		# Carga una librería en particular
-		#----------------------------------------
 		if (isset($url)) { 
-			$link = '<script type="text/javascript" href="'._JS_.$url.'" />';
-			return $link;
-
-		#-----------------------------------
-		# Carga todas las librerías 
-		#-----------------------------------
-
-		} else {
-			$files = array_diff(scandir(__JS__), array('..', '.'));!= "") $items[] = $i;
-			foreach ($files as $f):
-				$link += '<script type="text/javascript" href="'._JS_.$url.'" /><br/>';
-			endforeach;
-			return $link;
+			$link = '<script type="text/javascript" src="'._JS_.$url.'.js"></script>';
+			echo $link;
 		}
 	}
 
-	/*
-		example: $this->Html->img('smiley.jpg');
-		return: <script src="/img/smiley.jpg"></script>
-
-		$url = dirección de la imagen
-		$options = alt, width, height, style
-		
-	*/
-
-	function img($url,$options => array()) {
-		#----------------------------------------
-		# Carga una imagen en particular
-		#----------------------------------------
+	function image($url, $options) {
 		if (isset($url)) { 
-			if (!is_array($options)) { 
-				$alt = '':
-			} else { 
-				if (isset($options['alt']))
-					$alt = $options['alt'];
-				} 
-
-				if (isset($options['width'])) { 
-					$width = $options['width'];
-				}
-
-				if (isset($options['height'])) { 
-					$height = $options['height'];
-				}
-
-				if (isset($options['style'])) { 
-					$style = $options['style'];
-				}
+			$attributes = '';
+			foreach($options as $attr =>  $value){
+				$attributes = $attributes.' '.$attr.'="'.$value.'"';
 			}
-
-			$link = '<img src="'.__IMG__.$url.'" alt="'.$alt.'" width="'.$width'" height="'.$height.'" style="'.$style.'" />';
-			return $link;
+			$image = '<img src="'._IMAGES_.$url.'" '.$attributes.'>';
+			echo $image;
 		}
-	}
+		
+
+	}	
+
+	
+}
 ?>
--- index.php
+++ index.php
@@ -4,10 +4,21 @@
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', dirname(__FILE__));
 	define('ASSETS_DIR', 'assets');
-	define('WWW_ROOT', ROOT . DS . APP_DIR . DS . ASSETS_DIR . DS);
+	define('WWW_ROOT', $_SERVER['REQUEST_URI']. APP_DIR . DS . ASSETS_DIR . DS);
+
 	define('APP_CONFIG', ROOT . DS . 'config' . DS . 'app.php');
 	define('CONFIG_DB', ROOT . DS . 'config' . DS . 'config.php');
+
 	require(APP_DIR . DS . ASSETS_DIR . DS . 'index.php');
 	require(APP_CONFIG);
 	require(CONFIG_DB);
+	require(ROOT . DS . APP_DIR . DS . 'helpers/load_helper.php');
+	$html = new LoadHtml();
+	$html->css('foundation');
+	$html->javascript('vendor/jquery');
+	$html->javascript('foundation/foundation');
+	$html->image('logo-salfa.png',array('width' => 100));
 ?>
+
+
+
