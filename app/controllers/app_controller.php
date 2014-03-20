f864266b92ac9e41c5c0a60b3d36f58b7d1cc373
hiluxzero <info@jorgemora.cl>
2014-03-20 16:48:51 -0400

    Ultimos cambios

--- app/controllers/app_controller.php
+++ app/controllers/app_controller.php
@@ -1,11 +1,6 @@
 <?php 
+
 	Class Controller {
-		function render($controller,$view,$variables,$layout){
-			if(!empty($variables)) extract($variables);
-			ob_start();
-			require "views/".$controller."/".$view.".php";
-			$content = ob_get_clean();
-			require "views/layouts/".$layout.".php";
-		}
+		
 	}
 ?>
--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -1,21 +1,5 @@
 <?php
 	require("app_controller.php");
 	
-	class UsersController extends Controller{
-		function login(){
-
-		}	
-
-		function logout(){
-
-		}
-
-		function edit(){
-
-		}	
-
-		function changePassword(){
-
-		}
-	}
+	
 ?>
